e948ce904fd8151a9e6433a9f225a250359bb1ac
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-02-05 11:06:40 +0530

    statuscode test for api1 - get rating

--- test/routes/api1.js
+++ test/routes/api1.js
@@ -2,11 +2,21 @@ const server = require('../../server');
 const models = require('../../models');
 
 
-describe('test for api 1', () => {
-  test('should give valid status code', (done) => {
+describe('test for valid status code', () => {
+  test('should give valid status code for get allbooks', (done) => {
     const options = {
       method: 'GET',
       url: '/books',
+    };
+    server.inject(options, (response) => {
+      expect(response.statusCode).toEqual(200);
+      done();
+    });
+  });
+  test('should give valid status code for get ratings', (done) => {
+    const options = {
+      method: 'GET',
+      url: '/ratings/1',
     };
     server.inject(options, (response) => {
       expect(response.statusCode).toEqual(200);
