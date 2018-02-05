1d6d36a61ec7b8a768a29b5a84a7dff266c7add3
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-02-05 10:12:31 +0530

    server.js setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const Hapi = require('hapi');
+const Routes = require('./routes');
+
+const server = new Hapi.Server();
+
+server.connection({
+  port: 1313,
+  host: 'localhost',
+});
+server.route(Routes);
+
+if (!module.parent) {
+  server.start((err) => {
+    if (err) throw err;
+    console.log('Server started at port number 1313');
+  });
+}
+module.exports = server;
