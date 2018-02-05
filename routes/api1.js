52c70724fd7ee6ae3c5973c7c1a53ed95ad263b6
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-02-05 13:59:17 +0530

    I tried my best

--- routes/api1.js
+++ routes/api1.js
@@ -1,36 +1,28 @@
 const models = require('../models');
 const request = require('request-promise');
 const https = require('https');
-
+const getRating = (booksArray, response)=>{
+    let counter = 0;
+    booksArray.every((eachBook)=>{
+      request(`https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${eachBook.id}`)
+      .then((bookRating) => {
+        eachBook.rating = JSON.parse(bookRating).rating;
+        counter += 1;
+        if(counter === 12) response(booksArray)
+      })
+    })
+}
 module.exports = [
   {
     method: 'GET',
     path: '/books',
     handler: (req, response) => {
-      const promise = new Promise((rep,res)=>{
         request('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks')
         .then((booksData) => {
           return JSON.parse(booksData).books;
         })
-        .then((booksArray)=>{
-        // console.log(booksArray);
-          booksArray.every((eachBook)=>{
-          // console.log(eachBook);
-            request(`https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${eachBook.id}`)
-            .then((bookRating) => {
-              eachBook.rating = JSON.parse(bookRating).rating;
-            })
-          })
-        });
-      })
-      Promise.all(promise)
-      .then((finalResponse)=>{
-        console.log(finalResponse)
-        response({
-          statusCode: 200,
-          body: finalResponse,
-        });
-      });
+        .then(getrating, response);
     },
+
   },
 ]
