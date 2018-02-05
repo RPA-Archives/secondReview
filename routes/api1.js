3ea3899390736a1b024fdd28f07731bbbdc8f9b2
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-02-05 10:33:03 +0530

    statusCode test for api1 passed

--- routes/api1.js
+++ routes/api1.js
@@ -1,10 +1,17 @@
 const models = require('../models');
+const https = require('https');
 
 module.exports = [
   {
     method: 'GET',
-    path: '/',
+    path: '/books',
     handler: (request, response) => {
+      https.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', (reply) => {
+        // console.log(reply.statusCode);
+        response({
+          statusCode: reply.statusCode,
+        });
+      });
     },
   },
 ];
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-const api1 = require('./ap1');
-const api2 = require('./ap2');
-const api3 = require('./ap3');
+const api1 = require('./api1');
+const api2 = require('./api2');
+const api3 = require('./api3');
 
 module.exports = [].concat(api1, api2, api3);
